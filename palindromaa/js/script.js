d55099297b1cf3eb321537fc1e7a29f429373952
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-02-29 13:23:18 +0100

    correzione palindroma

--- palindromaa/js/script.js
+++ palindromaa/js/script.js
@@ -1,25 +1,58 @@
-let parola= prompt('inserisci una parola')
-console.log(parola)
 
-let parolaInversa= inverto(parola)
-console.log(parolaInversa)
-if(parola===parolaInversa){
-  console.log('la parola è palindroma ')
-  
+const parola= prompt('inserisci parola')
+
+console.log(parola)
+const parolaGirata= inverto(parola);
+console.log(parolaGirata)
+
+
+if(ispalindroma(parola,parolaGirata)){
+  console.log('la parola è palindroma')
 }
 else{
-  console.log('la parola non è palindroma')
+  console.log('la parola non è palindroma ')
 }
 
 
 
-function inverto(parola ){
- let parolaDivisa= parola.split('')
- console.log(parolaDivisa)
- let arrayinveso= parolaDivisa.reverse()
- console.log(arrayinveso)
- return arrayinveso.join('')
+
+function inverto(parolaDaGirare){
+
+  let parolaAlContrario="";
+  for(let i=parolaDaGirare.length-1; i>=0; i-- ){
+    parolaAlContrario += parolaDaGirare[i];
+  }
+
+  return parolaAlContrario 
 }
 
+  function ispalindroma(parolaOriginale, parolaRigirata){
+    return parolaOriginale=== parolaRigirata;
+  }
 
+
+
+
+// let parolaInversa= inverto(parola)
+// console.log(parolaInversa)
+// if(parola===parolaInversa){
+//   console.log('la parola è palindroma ')
+  
+// }
+// else{
+//   console.log('la parola non è palindroma')
+// }
+
+
+
+// function inverto(parola ){
+//  let parolaDivisa= parola.split('')
+//  console.log(parolaDivisa)
+//  let arrayinveso= parolaDivisa.reverse()
+//  console.log(arrayinveso)
+//  return arrayinveso.join('')
+// // }
+// const parolaDivisa= parola.split('')
+// console.log(parolaDivisa)
+// inverto(parolaDivisa)
 
